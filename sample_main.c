a02612e74ac82e3344c0f4bd18ce78a28fd97e60
nunoya-yuma <nuno.yu.3838@gmail.com>
2024-07-31 00:17:19 +0900

    Manage status with enums instead of strings

--- sample_main.c
+++ sample_main.c
@@ -2,9 +2,42 @@
 
 #include "nn_linenoise.h"
 
-static char *s_sample_status = "OFF";
+typedef enum
+{
+    SAMPLE_STATUS_INVALID,
+    SAMPLE_STATUS_ON,
+    SAMPLE_STATUS_OFF,
+} SampleStatus_t;
 
-int sample_show_cmd(int argc, char **argv)
+SampleStatus_t s_sample_status = SAMPLE_STATUS_INVALID;
+
+static const char *GetStringFromSampleStatus(SampleStatus_t a_status)
+{
+    const char *status_str = "Error";
+
+    switch (a_status)
+    {
+    case SAMPLE_STATUS_INVALID:
+        status_str = "Invalid";
+        break;
+
+    case SAMPLE_STATUS_ON:
+        status_str = "ON";
+        break;
+
+    case SAMPLE_STATUS_OFF:
+        status_str = "OFF";
+        break;
+
+    default:
+        printf("[NN] Error: Unexpected status\n");
+        break;
+    }
+
+    return status_str;
+}
+
+static int Sample_ShowStatusCmd(int argc, char **argv)
 {
     int res = 0;
     if (argc != 1)
@@ -13,7 +46,8 @@ int sample_show_cmd(int argc, char **argv)
         res = 1;
         goto done;
     }
-    printf("Sample status: %s\n", s_sample_status);
+
+    printf("Sample status: %s\n", GetStringFromSampleStatus(s_sample_status));
 
 done:
     return res;
@@ -22,7 +56,7 @@ done:
 int main(int argc, char **argv)
 {
     NN_LinenoiseRegisterCommand_t sample_cmd_config = {
-        .m_func = sample_show_cmd,
+        .m_func = Sample_ShowStatusCmd,
         .m_command_name = "sample-status",
         .m_help_msg = "Show current sample status: <ON/OFF>",
     };
