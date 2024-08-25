63fec6fe8c4ad74daa3642dae1d996e6e0cf6080
nunoya-yuma <nuno.yu.3838@gmail.com>
2024-08-25 15:00:20 +0900

    Prepare config file for custom settings

---
+++ examples/nn_cli_config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Example)
+// #define NNCli_LogError(fmt, ...) fprintf(stderr, "[SAMPLE][ERROR]" fmt "\n", ##__VA_ARGS__)
--- nn_cli.h
+++ nn_cli.h
@@ -1,21 +1,5 @@
 #pragma once
 
-#ifndef NN_CLI__MAX_COMMAND_NUM
-#define NN_CLI__MAX_COMMAND_NUM 200
-#endif
-
-#ifndef NNCli_LogInfo
-#define NNCli_LogInfo(fmt, ...) printf("[NNCli][INFO]" fmt "\n", ##__VA_ARGS__)
-#endif
-
-#ifndef NNCli_LogWarn
-#define NNCli_LogWarn(fmt, ...) printf("[NNCli][WARN]" fmt "\n", ##__VA_ARGS__)
-#endif
-
-#ifndef NNCli_LogError
-#define NNCli_LogError(fmt, ...) fprintf(stderr, "[NNCli][ERROR]" fmt "\n", ##__VA_ARGS__)
-#endif
-
 typedef int (*NNCli_Command_t)(int argc, char **argv);
 
 typedef struct
