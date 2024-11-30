0f1b8d38c1b1c9cdd7621f5ae21f72d89f6a2987
nunoya-yuma <nuno.yu.3838@gmail.com>
2024-11-30 23:05:40 +0900

    Update test cases for improved coverage and edge case handling

--- test/nn_cli_test.cpp
+++ test/nn_cli_test.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 
 #include "nn_cli.h"
+#include "nn_cli_config.h"
 
+#include "nn_cli.c"
 namespace
 {
     NNCli_Err_t TestCmdFunc(int argc, char **argv)
@@ -9,22 +11,113 @@ namespace
         // Nothing to do
         return NN_CLI__SUCCESS;
     }
-}
+} // namespace
+
+class NNCliTest : public ::testing::Test
+{
+protected:
+    void TearDown() override
+    {
+        s_async = {0};
+        s_command_list = {0};
+    }
+};
 
 namespace testing
 {
-    TEST(NNCliTest, RegisterCommand)
+    TEST_F(NNCliTest, RegisterCommand_Success)
     {
-        static const NNCli_Command_t cmd = {
-            /* .m_func = */ TestCmdFunc,
-            /* .m_name = */ "test-cmd",
-            /* .m_help_msg = */ "test help msg",
-            /* .m_options = */ "",
+        constexpr NNCli_Command_t cmd = {
+            .m_func = TestCmdFunc,
+            .m_name = "test-cmd",
+            .m_options = "on/off",
+            .m_help_msg = "test help msg",
         };
 
         EXPECT_EQ(NNCli_RegisterCommand(&cmd), NN_CLI__SUCCESS);
 
-        EXPECT_EQ(NNCli_RegisterCommand(nullptr), NN_CLI__INVALID_ARGS);
+        constexpr NNCli_Command_t no_option_cmd = {
+            .m_func = TestCmdFunc,
+            .m_name = "test-cmd",
+            .m_options = nullptr,
+            .m_help_msg = "test help msg",
+        };
+        EXPECT_EQ(NNCli_RegisterCommand(&no_option_cmd), NN_CLI__SUCCESS);
     }
 
-} // testing
+    TEST_F(NNCliTest, RegisterCommand_InvalidArgs)
+    {
+        EXPECT_EQ(NNCli_RegisterCommand(nullptr), NN_CLI__INVALID_ARGS);
+
+        constexpr NNCli_Command_t no_func_cmd = {
+            .m_func = nullptr,
+            .m_name = "test-cmd",
+            .m_options = "on/off",
+            .m_help_msg = "test help msg",
+        };
+        EXPECT_EQ(NNCli_RegisterCommand(&no_func_cmd), NN_CLI__INVALID_ARGS);
+
+        constexpr NNCli_Command_t no_name_cmd = {
+            .m_func = TestCmdFunc,
+            .m_name = "",
+            .m_options = "on/off",
+            .m_help_msg = "test help msg",
+        };
+        EXPECT_EQ(NNCli_RegisterCommand(&no_name_cmd), NN_CLI__INVALID_ARGS);
+    }
+
+    TEST_F(NNCliTest, RegisterCommand_upperLimit)
+    {
+        constexpr NNCli_Command_t cmd = {
+            .m_func = TestCmdFunc,
+            .m_name = "test-cmd",
+            .m_options = "on/off",
+            .m_help_msg = "test help msg",
+        };
+
+        for (int i = 0; i < NN_CLI__MAX_COMMAND_NUM; i++)
+        {
+            EXPECT_EQ(NNCli_RegisterCommand(&cmd), NN_CLI__SUCCESS);
+        }
+
+        EXPECT_EQ(NNCli_RegisterCommand(&cmd), NN_CLI__EXCEED_CAPACITY);
+    }
+
+    TEST_F(NNCliTest, NNCliTest_RegisterCommand_InvalidArgs)
+    {
+        EXPECT_EQ(NNCli_RegisterCommand(nullptr), NN_CLI__INVALID_ARGS);
+
+        constexpr NNCli_Command_t no_func_cmd = {
+            .m_func = nullptr,
+            .m_name = "test-cmd",
+            .m_options = "on/off",
+            .m_help_msg = "test help msg",
+        };
+        EXPECT_EQ(NNCli_RegisterCommand(&no_func_cmd), NN_CLI__INVALID_ARGS);
+
+        constexpr NNCli_Command_t no_name_cmd = {
+            .m_func = TestCmdFunc,
+            .m_name = "",
+            .m_options = "on/off",
+            .m_help_msg = "test help msg",
+        };
+        EXPECT_EQ(NNCli_RegisterCommand(&no_name_cmd), NN_CLI__INVALID_ARGS);
+    }
+
+    TEST_F(NNCliTest, Init_Success)
+    {
+        const NNCli_Option_t option = {
+            .m_enable_multi_line = true,
+            .m_show_key_codes = false,
+            .m_async = {
+                .m_enabled = false,
+                .m_timeout = {
+                    .tv_sec = 0,
+                    .tv_usec = 0},
+            },
+            .m_history_filename = "test_history.txt"};
+
+        EXPECT_EQ(NNCli_Init(&option), NN_CLI__SUCCESS);
+        free(s_history_filename);
+    }
+} // namespace testing
