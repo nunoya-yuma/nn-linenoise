97292d615c91406fa07ec100c00a032e867b19f2
nunoya-yuma <nuno.yu.3838@gmail.com>
2024-12-01 01:37:55 +0900

    Update tests for NNCli_Run()
    - Add test for Run
    - Prepare a function that imitates keyboard input
    - EXPECT_EQ -> ASSERT_EQ

--- test/nn_cli_test.cpp
+++ test/nn_cli_test.cpp
@@ -11,6 +11,30 @@ namespace
         // Nothing to do
         return NN_CLI__SUCCESS;
     }
+
+    void DummyKeyboardInput(const char *input)
+    {
+        char filename[] = "/tmp/nncli_testXXXXXX";
+        int fd = mkstemp(filename);
+        if (fd == -1)
+        {
+            perror("mkstemp failed");
+            assert(0);
+        }
+        close(fd);
+
+        FILE *fake_stdin = fopen(filename, "w+");
+        if (!fake_stdin)
+        {
+            perror("fopen failed");
+            assert(0);
+        }
+
+        fprintf(fake_stdin, "%s", input);
+        fclose(fake_stdin);
+
+        freopen(filename, "r", stdin);
+    }
 } // namespace
 
 class NNCliTest : public ::testing::Test
@@ -34,7 +58,7 @@ namespace testing
             .m_help_msg = "test help msg",
         };
 
-        EXPECT_EQ(NNCli_RegisterCommand(&cmd), NN_CLI__SUCCESS);
+        ASSERT_EQ(NNCli_RegisterCommand(&cmd), NN_CLI__SUCCESS);
 
         constexpr NNCli_Command_t no_option_cmd = {
             .m_func = TestCmdFunc,
@@ -42,12 +66,12 @@ namespace testing
             .m_options = nullptr,
             .m_help_msg = "test help msg",
         };
-        EXPECT_EQ(NNCli_RegisterCommand(&no_option_cmd), NN_CLI__SUCCESS);
+        ASSERT_EQ(NNCli_RegisterCommand(&no_option_cmd), NN_CLI__SUCCESS);
     }
 
     TEST_F(NNCliTest, RegisterCommand_InvalidArgs)
     {
-        EXPECT_EQ(NNCli_RegisterCommand(nullptr), NN_CLI__INVALID_ARGS);
+        ASSERT_EQ(NNCli_RegisterCommand(nullptr), NN_CLI__INVALID_ARGS);
 
         constexpr NNCli_Command_t no_func_cmd = {
             .m_func = nullptr,
@@ -55,7 +79,7 @@ namespace testing
             .m_options = "on/off",
             .m_help_msg = "test help msg",
         };
-        EXPECT_EQ(NNCli_RegisterCommand(&no_func_cmd), NN_CLI__INVALID_ARGS);
+        ASSERT_EQ(NNCli_RegisterCommand(&no_func_cmd), NN_CLI__INVALID_ARGS);
 
         constexpr NNCli_Command_t no_name_cmd = {
             .m_func = TestCmdFunc,
@@ -63,7 +87,7 @@ namespace testing
             .m_options = "on/off",
             .m_help_msg = "test help msg",
         };
-        EXPECT_EQ(NNCli_RegisterCommand(&no_name_cmd), NN_CLI__INVALID_ARGS);
+        ASSERT_EQ(NNCli_RegisterCommand(&no_name_cmd), NN_CLI__INVALID_ARGS);
     }
 
     TEST_F(NNCliTest, RegisterCommand_upperLimit)
@@ -77,15 +101,15 @@ namespace testing
 
         for (int i = 0; i < NN_CLI__MAX_COMMAND_NUM; i++)
         {
-            EXPECT_EQ(NNCli_RegisterCommand(&cmd), NN_CLI__SUCCESS);
+            ASSERT_EQ(NNCli_RegisterCommand(&cmd), NN_CLI__SUCCESS);
         }
 
-        EXPECT_EQ(NNCli_RegisterCommand(&cmd), NN_CLI__EXCEED_CAPACITY);
+        ASSERT_EQ(NNCli_RegisterCommand(&cmd), NN_CLI__EXCEED_CAPACITY);
     }
 
     TEST_F(NNCliTest, NNCliTest_RegisterCommand_InvalidArgs)
     {
-        EXPECT_EQ(NNCli_RegisterCommand(nullptr), NN_CLI__INVALID_ARGS);
+        ASSERT_EQ(NNCli_RegisterCommand(nullptr), NN_CLI__INVALID_ARGS);
 
         constexpr NNCli_Command_t no_func_cmd = {
             .m_func = nullptr,
@@ -93,7 +117,7 @@ namespace testing
             .m_options = "on/off",
             .m_help_msg = "test help msg",
         };
-        EXPECT_EQ(NNCli_RegisterCommand(&no_func_cmd), NN_CLI__INVALID_ARGS);
+        ASSERT_EQ(NNCli_RegisterCommand(&no_func_cmd), NN_CLI__INVALID_ARGS);
 
         constexpr NNCli_Command_t no_name_cmd = {
             .m_func = TestCmdFunc,
@@ -101,7 +125,7 @@ namespace testing
             .m_options = "on/off",
             .m_help_msg = "test help msg",
         };
-        EXPECT_EQ(NNCli_RegisterCommand(&no_name_cmd), NN_CLI__INVALID_ARGS);
+        ASSERT_EQ(NNCli_RegisterCommand(&no_name_cmd), NN_CLI__INVALID_ARGS);
     }
 
     TEST_F(NNCliTest, Init_Success)
@@ -117,13 +141,13 @@ namespace testing
             },
             .m_history_filename = "test_history.txt"};
 
-        EXPECT_EQ(NNCli_Init(&option), NN_CLI__SUCCESS);
+        ASSERT_EQ(NNCli_Init(&option), NN_CLI__SUCCESS);
         free(s_history_filename);
     }
 
     TEST_F(NNCliTest, Init_InvalidArgs)
     {
-        EXPECT_EQ(NNCli_Init(nullptr), NN_CLI__INVALID_ARGS);
+        ASSERT_EQ(NNCli_Init(nullptr), NN_CLI__INVALID_ARGS);
 
         const NNCli_Option_t option = {
             .m_enable_multi_line = true,
@@ -135,6 +159,34 @@ namespace testing
                     .tv_usec = 0},
             },
             .m_history_filename = ""};
-        EXPECT_EQ(NNCli_Init(&option), NN_CLI__INVALID_ARGS);
+        ASSERT_EQ(NNCli_Init(&option), NN_CLI__INVALID_ARGS);
+    }
+
+    TEST_F(NNCliTest, Run_Success)
+    {
+
+        constexpr NNCli_Command_t cmd = {
+            .m_func = TestCmdFunc,
+            .m_name = "test-cmd",
+            .m_options = "on/off",
+            .m_help_msg = "test help msg",
+        };
+
+        ASSERT_EQ(NNCli_RegisterCommand(&cmd), NN_CLI__SUCCESS);
+        const NNCli_Option_t option = {
+            .m_enable_multi_line = true,
+            .m_show_key_codes = false,
+            .m_async = {
+                .m_enabled = false,
+                .m_timeout = {
+                    .tv_sec = 0,
+                    .tv_usec = 0},
+            },
+            .m_history_filename = "test_history.txt"};
+
+        ASSERT_EQ(NNCli_Init(&option), NN_CLI__SUCCESS);
+
+        DummyKeyboardInput("help\n");
+        ASSERT_EQ(NNCli_Run(), NN_CLI__SUCCESS);
     }
 } // namespace testing
